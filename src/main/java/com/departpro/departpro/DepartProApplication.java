65958e4f436c95d4a06453317d9d7a4f6461ed07
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-09-03 19:27:53 +0300

    Add starter files

---
+++ src/main/java/com/departpro/departpro/DepartProApplication.java
@@ -0,0 +1,13 @@
+package com.departpro.departpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartProApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/departpro/departpro/DepartProApplicationTests.java
@@ -0,0 +1,13 @@
+package com.departpro.departpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DepartProApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
