fc04e04772fdc24a17d6401c66194f4ceaf2be0b
DanielJHV <120484672+DanielJHV@users.noreply.github.com>
2023-09-07 14:14:02 +0300

    Add GET method and ResourceNotFoundException

--- src/main/java/com/danieljhv/departpro/controller/EmployeeController.java
+++ src/main/java/com/danieljhv/departpro/controller/EmployeeController.java
@@ -4,15 +4,12 @@ import com.danieljhv.departpro.dto.EmployeeDto;
 import com.danieljhv.departpro.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
-    private final EmployeeService employeeService;
+    private EmployeeService employeeService;
 
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
@@ -22,6 +19,11 @@ public class EmployeeController {
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto) {
         EmployeeDto savedEmployee = employeeService.createEmployee(employeeDto);
         return new ResponseEntity<>(savedEmployee, HttpStatus.CREATED);
+    }
 
+    @GetMapping("{id}")
+    public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId) {
+        EmployeeDto employeeDto = employeeService.getEmployeeById(employeeId);
+        return ResponseEntity.ok(employeeDto);
     }
 }
--- src/main/java/com/danieljhv/departpro/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/danieljhv/departpro/service/impl/EmployeeServiceImpl.java
@@ -1,6 +1,7 @@
 package com.danieljhv.departpro.service.impl;
 
 import com.danieljhv.departpro.dto.EmployeeDto;
+import com.danieljhv.departpro.exception.ResourceNotFoundException;
 import com.danieljhv.departpro.mapper.EmployeeMapper;
 import com.danieljhv.departpro.repository.EmployeeRepository;
 import com.danieljhv.departpro.entity.Employee;
@@ -8,13 +9,20 @@ import com.danieljhv.departpro.service.EmployeeService;
 import org.springframework.stereotype.Service;
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
-    private EmployeeRepository employeeRepository;
+    private final EmployeeRepository employeeRepository;
     @Override
     public EmployeeDto createEmployee(EmployeeDto employeeDto) {
 
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException("No employee available with that id: " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 
     public EmployeeServiceImpl(EmployeeRepository employeeRepository) {
